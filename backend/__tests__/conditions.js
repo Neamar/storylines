07b5c19d4935dd402e6c6c3ad8449da6e566b63d
Neamar <neamar@neamar.fr>
2018-01-21 14:15:32 +0000

    Added tests for conditions and operations

--- backend/__tests__/conditions.js
+++ backend/__tests__/conditions.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const conditions = require('../conditions.js');
+
+
+describe("conditions", () => {
+  describe("parseCondition()", () => {
+    test("should parse valid condition", () => {
+      expect(conditions.parseCondition('global.something == true')).toEqual({
+        lhs: ["@", "global", "something"],
+        operator: "==",
+        rhs: true
+      });
+    });
+
+    test("should require a valid comparison operator", () => {
+      expect(() => conditions.parseCondition('global.something += true')).toThrow(/Invalid operator \+=/i);
+    });
+
+  });
+});
--- backend/__tests__/operations.js
+++ backend/__tests__/operations.js
@@ -1,1 +1,24 @@
 "use strict";
+
+const operations = require('../operations.js');
+
+
+describe("operations", () => {
+  describe("parseOperation()", () => {
+    test("should parse valid operation", () => {
+      expect(operations.parseOperation('global.something = true')).toEqual({
+        lhs: ["@", "global", "something"],
+        operator: "=",
+        rhs: true
+      });
+    });
+
+    test("should require a valid assignment operator", () => {
+      expect(() => operations.parseOperation('global.something == true')).toThrow(/Invalid operator ==/i);
+    });
+
+    test("should require lhs to be a state access", () => {
+      expect(() => operations.parseOperation('true == true')).toThrow(/Operation lhs must change state/i);
+    });
+  });
+});
