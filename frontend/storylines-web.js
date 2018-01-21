67cccb3ad31d2b5f78acd043ff4e9ab175f60162
Neamar <neamar@neamar.fr>
2018-01-21 18:12:37 +0000

    Add HTML and JS skeleton

---
+++ frontend/storylines-web.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function displayEvent(event) {
+  let eventHTML = `
+<div class="jumbotron">
+  <p>${event.description}</p>
+  <p class="lead">
+`;
+
+  Object.keys(event.actions).forEach(a => {
+    eventHTML += `<a class="btn btn-primary btn-lg" href="#" role="button">${a}</a>`;
+  });
+
+  eventHTML += `</p>
+</div>
+`;
+
+  document.getElementById('event').innerHTML = eventHTML;
+}
---
+++ frontend/storylines.js
@@ -0,0 +1,49 @@
+class Storylines {
+  constructor(story) {
+    this.story = story;
+    this.events = story.events;
+    this.resources = story.resources;
+    this.title = story.story_title;
+    this.description = story.story_description;
+
+    // Clone default state
+    this.state = Object.assign({}, story.default_state);
+  }
+
+  listAvailableEvents(triggerType) {
+    return this.events;
+  }
+
+  listAvailableHardEvents() {
+    return this.listAvailableEvents("hard");
+  }
+
+  listAvailableSoftEvents() {
+    return this.listAvailableEvents("soft");
+  }
+
+  nextEvent() {
+    var hardEvents = this.listAvailableHardEvents();
+    if(hardEvents.length > 0) {
+      this.moveToEvent(hardEvents[0]);
+      return;
+    }
+
+    var softEvents = this.listAvailableSoftEvents();
+    if(softEvents.length > 0) {
+      return;
+    }
+
+    if(!this.state.global.no_events_available) {
+      this.state.global.no_events_available = true;
+      this.nextEvent();
+      return;
+    }
+
+    throw new Error("No more events available, no listeners on no_events_available.");
+  }
+
+  moveToEvent(event) {
+    displayEvent(event);
+  }
+}
