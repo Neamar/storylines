5ba8677651f0d091f4d315d5546ebc472bbecd6e
Neamar <neamar@neamar.fr>
2018-05-26 17:04:01 +0100

    Rename graph to chart

--- bin/storylines.js
+++ bin/storylines.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 'use strict';
 
 const program = require('commander');
@@ -5,6 +6,6 @@ const program = require('commander');
 program
   .version(require('../package.json').version)
   .command('compile <story> [output]', 'compile the story and write to stdout or output')
-  .command('graph <story> [raw] [dot]', 'graph all possible options and write to raw and dot (graphviz) files')
+  .command('chart <story> [raw] [dot]', 'chart all possible options and write to raw and dot (graphviz) files')
   .command('local <path>', 'run the given story in a local server')
   .parse(process.argv);
